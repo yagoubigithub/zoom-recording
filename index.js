5af8f583f0d7571024fc4a944845935adbd8a5eb
abdelkader <yagoubi.aek.2@gmail.com>
2024-09-24 13:47:30 +0100

    socket.io

---
+++ index.js
@@ -0,0 +1,36 @@
+// Import dependencies
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+const cors = require('cors');
+
+// Create the app and server
+const app = express();
+app.use(cors({
+    origin: '*' // Allow only the client with entering the url of client
+}));
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.get("/", (req, res) => {
+    res.send("Server is running.");
+});
+
+// Handle new socket connections
+io.on('connection', (socket) => {
+
+    // Handle incoming audio stream
+    socket.on('audioStream', (audioData) => {
+        console.log(audioData)
+        socket.broadcast.emit('audioStream', audioData);
+    });
+
+    socket.on('disconnect', () => {
+    });
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
